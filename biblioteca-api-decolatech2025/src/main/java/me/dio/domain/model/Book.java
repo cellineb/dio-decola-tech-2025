4733f58e5cc7e07bc7b43364d09f9c9783c45480
Celline Bitencourt <cellinebitencourt@gmail.com>
2025-03-05 14:10:14 -0300

    Adicionando as camadas model e repository, e conectando o banco de dados

---
+++ biblioteca-api-decolatech2025/src/main/java/me/dio/domain/model/Book.java
@@ -0,0 +1,70 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity(name = "tb_book")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String title;
+    private String author;
+    private String genre;
+    private LocalDate publicationDate;
+    private int stars;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public LocalDate getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(LocalDate publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    public void setStars(int stars) {
+        this.stars = stars;
+    }
+}
---
+++ biblioteca-api-decolatech2025/src/main/java/me/dio/domain/model/User.java
@@ -0,0 +1,73 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String CPF;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "user_favorites", // Nome da tabela de junção
+            joinColumns = @JoinColumn(name = "user_id"), // Coluna do usuário
+            inverseJoinColumns = @JoinColumn(name = "book_id") // Coluna do livro
+    )
+    private List<Book> favorites = new ArrayList<>();
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "user_wishlist", // Nome da tabela de junção
+            joinColumns = @JoinColumn(name = "user_id"), // Coluna do usuário
+            inverseJoinColumns = @JoinColumn(name = "book_id") // Coluna do livro
+    )
+    private List<Book> wishlist = new ArrayList<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public List<Book> getFavorites() {
+        return favorites;
+    }
+
+    public void setFavorites(List<Book> favorites) {
+        this.favorites = favorites;
+    }
+
+    public List<Book> getWishlist() {
+        return wishlist;
+    }
+
+    public void setWishlist(List<Book> wishlist) {
+        this.wishlist = wishlist;
+    }
+}
