44deb09186f240d5ae4605d6385e96f67cf894ca
Celline Bitencourt <cellinebitencourt@gmail.com>
2025-03-05 22:05:13 -0300

    Finalizando projeto

--- biblioteca-api-decolatech2025/src/main/java/me/dio/BibliotecaApiDecolatech2025Application.java
+++ biblioteca-api-decolatech2025/src/main/java/me/dio/BibliotecaApiDecolatech2025Application.java
@@ -1,8 +1,13 @@
 package me.dio;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.Serial;
+
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class BibliotecaApiDecolatech2025Application {
 
