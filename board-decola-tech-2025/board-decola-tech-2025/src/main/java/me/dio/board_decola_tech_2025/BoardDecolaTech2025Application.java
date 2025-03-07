50cdf280f64c0fa481a6df4ea458cf26f7c68232
Celline Bitencourt <cellinebitencourt@gmail.com>
2025-03-07 18:04:39 -0300

    Iniciando Projeto 2

---
+++ board-decola-tech-2025/board-decola-tech-2025/src/main/java/me/dio/board_decola_tech_2025/BoardDecolaTech2025Application.java
@@ -0,0 +1,13 @@
+package me.dio.board_decola_tech_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardDecolaTech2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardDecolaTech2025Application.class, args);
+	}
+
+}
---
+++ board-decola-tech-2025/board-decola-tech-2025/src/main/java/me/dio/board_decola_tech_2025/persistence/config/ConnectionConfig.java
@@ -0,0 +1,4 @@
+package me.dio.board_decola_tech_2025.persistence.config;
+
+public class ConnectionConfig {
+}
