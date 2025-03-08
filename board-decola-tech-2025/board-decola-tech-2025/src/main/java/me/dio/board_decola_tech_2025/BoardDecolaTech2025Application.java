4d3ff54aef9ba470ebf62d37bbc4016d47f71e4d
Celline Bitencourt <cellinebitencourt@gmail.com>
2025-03-08 00:13:04 -0300

    conxeção com o banco de dados

--- board-decola-tech-2025/board-decola-tech-2025/src/main/java/me/dio/board_decola_tech_2025/BoardDecolaTech2025Application.java
+++ board-decola-tech-2025/board-decola-tech-2025/src/main/java/me/dio/board_decola_tech_2025/BoardDecolaTech2025Application.java
@@ -1,13 +1,20 @@
 package me.dio.board_decola_tech_2025;
 
+import me.dio.board_decola_tech_2025.persistence.migration.MigrationStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import java.sql.SQLException;
+
+import static me.dio.board_decola_tech_2025.persistence.config.ConnectionConfig.getConnection;
+
+
 public class BoardDecolaTech2025Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BoardDecolaTech2025Application.class, args);
-	}
+	public static void main(String[] args) throws SQLException {
 
+		try (var connection = getConnection()) {
+			new MigrationStrategy(connection).executeMigration();
+		}
+	}
 }
