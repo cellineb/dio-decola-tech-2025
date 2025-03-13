0776ddd7e18ad5d89965ac1f58bda86e0c6647a9
Celline Bitencourt <cellinebitencourt@gmail.com>
2025-03-12 21:39:53 -0300

    criando ambiente barber-api

---
+++ barbearia-decola-tech-2025/barber-api/barber-api/src/main/java/me/dio/barber_api/BarberApiApplication.java
@@ -0,0 +1,13 @@
+package me.dio.barber_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberApiApplication.class, args);
+	}
+
+}
